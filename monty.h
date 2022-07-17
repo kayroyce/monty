c0c108cda02f725246b629cfd1ad26cf38a2fa9e
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-07-17 21:09:34 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,11 +1,30 @@
-#ifndef MONTY_H_
-#define MONTY_H_
-#define  _GNU_SOURCE
+#ifndef _MONTY_H_
+#define _MONTY_H_
 
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * struct var_s - struct to contain the main variables of the Monty interpreter
+ * @queue: flag to determine if in stack vs queue mode
+ * @stack_len: length of the stack
+ */
+typedef struct var_s
+{
+	int queue;
+	size_t stack_len;
+} var_t;
+
+#define STACK 0
+#define QUEUE 1
+
+/* global struct to hold flag for queue and stack length */
+extern var_t var;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -14,80 +33,51 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
-
 typedef struct stack_s
 {
 	int n;
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
+
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
-/**
- * struct glob_s - global and its funcs
- * @fd: File descriptor
- * @line: Line to getline
- *
- * Description: To handle the file and getline
- */
-typedef struct glob_s
-{
-	FILE *fd;
-	char *line;
-} glob_t;
-
-/* globals */
-extern glob_t global;
-extern int value;
-
-/* utilities */
-
-/* create new node */
-stack_t *NewNode(int value);
-
-/* check if stack is empty */
-int isEmpty(stack_t **stack);
-
-int isvalid_digit(char *arg);
-
-/* get_opcode.c */
-int get_opc(stack_t **stack, char *arg, char *item, int count);
-
-/* operations- SET A */
+void get_op(char *op, stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
+void _push2(stack_t **stack, int n);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
-
-/* operations- SET B */
 void _swap(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void _stack(stack_t **stack, unsigned int line_number);
+void _queue(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+void free_stack(int status, void *arg);
+void _fs_close(int status, void *arg);
+void free_lineptr(int status, void *arg);
+stack_t *add_node(stack_t **stack, const int n);
 
-/* cleaup functions */
-void free_stack(stack_t *stack);
-void free_lineptr(void *arg);
-
-/* monty.c */
-void handle_command(char *argv);
-
-/* error handler */
-void push_failure(FILE *fd, char *line, stack_t *stack, int count);
-void no_command(FILE *fd, char *line, stack_t *stack, char *count, int item);
-
-#endif
+#endif /* _MONTY_H_ */
